Simplified output shape computation. Fixed layer incompatibilities.

src/layers/routing_capsules.py
@@ -58,6 +58,8 @@ class RoutingCapsules(nn.Module):
 
     def forward(self, x):
         batch_size = x.size(0)
+        device = x.device
+
         # Unsqueeze to (batch_size, 1, input_caps, input_dim, 1)
         x = x.unsqueeze(1).unsqueeze(4)
 
@@ -81,7 +83,7 @@ class RoutingCapsules(nn.Module):
         """
 
         b = torch.zeros(batch_size, self.num_class_caps, self.num_input_caps, 1).to(
-            self.device
+            device
         )
         for r in range(self.routing_steps - 1):
             if self.routing_algorithm == "sigmoid":
@@ -101,7 +103,3 @@ class RoutingCapsules(nn.Module):
         v = squash(s)
 
         return v
-
-    def get_output_shape(self, input_shape):
-        # Output shape is (batch_size, num_class_caps, capsule_dimension)
-        return (input_shape[0], self.num_class_caps, self.capsule_dimension)

src/models/model_conv_attributes_32.py
@@ -10,6 +10,8 @@ from layers import (
 from numpy import prod
 from utils.layer_output_shape import get_network_output_shape
 
+_PRIMARY_CAPS_IDX = 1
+
 
 class CapsuleNetworkWithAttributes32(nn.Module):
     """
@@ -61,17 +63,10 @@ class CapsuleNetworkWithAttributes32(nn.Module):
             padding="valid",
         )
 
-        encoder_output_shape = get_network_output_shape(
-            (1, *img_shape),  # Add fictitious batch dimension to the input tensor
-            encoder_layers,
-            print_all=True,
+        capsules_output_shape = get_network_output_shape(
+            (1, *img_shape), [*encoder_layers, self.primary], print_all=True
         )
-        capsules_output_shape = self.primary.get_output_shape(encoder_output_shape)
-        print(f"Capsules output shape: {capsules_output_shape}")
-        _, _, h_caps, w_caps = capsules_output_shape
-        primary_caps = self.primary._caps_num * h_caps * w_caps
-
-        print(f"Capsules params: {w_caps} X {h_caps} X {primary_caps}")
+        primary_caps = capsules_output_shape[_PRIMARY_CAPS_IDX]
 
         self.digits = RoutingCapsules(
             primary_dim,
@@ -88,17 +83,6 @@ class CapsuleNetworkWithAttributes32(nn.Module):
             capsule_dim=output_dim,
             output_dim=num_classes,
         )
-
-        # Decoder
-        # decoder_layers = [
-        #     nn.Linear(output_dim * num_attributes, 512),
-        #     nn.ReLU(inplace=True),
-        #     nn.Linear(512, 1024),
-        #     nn.ReLU(inplace=True),
-        #     nn.Linear(1024, int(prod(img_shape))),
-        #     nn.Sigmoid(),
-        # ]
-        # self.decoder = nn.Sequential(*decoder_layers)
 
         self.decoder_layers = [
             nn.ConvTranspose2d(256, 256, kernel_size=7, stride=1, padding=1),
@@ -120,9 +104,10 @@ class CapsuleNetworkWithAttributes32(nn.Module):
             fmap_width=12,
             layers=self.decoder_layers,
         )
-        encoder_output_shape = get_network_output_shape(
+
+        _ = get_network_output_shape(
             capsules_output_shape,
-            self.decoder_layers,
+            [self.digits, self.decoder],
             print_all=True,
         )
 

src/utils/layer_output_shape.py
@@ -1,129 +1,4 @@
-from torch import nn
-
-
-def unpack_params(kernel_size, stride, padding):
-    if isinstance(kernel_size, tuple):
-        kernel_height, kernel_width = kernel_size
-    else:
-        kernel_height = kernel_width = kernel_size
-
-    if isinstance(stride, tuple):
-        stride_height, stride_width = stride
-    else:
-        stride_height = stride_width = stride
-
-    if isinstance(padding, tuple):
-        pad_h, pad_w = padding
-    else:
-        pad_h = pad_w = padding
-
-    return (
-        kernel_height,
-        kernel_width,
-        stride_height,
-        stride_width,
-        pad_h,
-        pad_w,
-    )
-
-
-def get_maxpool2d_output_shape(input_shape, kernel_size, stride, padding="valid"):
-    """
-    Utility function to calculate the output shape of a MaxPool2D layer
-
-    Args:
-        input_shape (tuple): The input shape of the layer
-        kernel_size (int): The size of the kernel
-        stride (int): The stride of the layer
-        padding (str): The padding of the layer
-
-    Returns:
-        int: The output shape of the layer
-    """
-    # Transform padding from string to int
-    if isinstance(padding, str):
-        if padding == "valid":
-            padding = 0
-        elif padding == "same":
-            padding = (kernel_size - 1) // 2
-        else:
-            raise ValueError(f"Invalid padding type: {padding}")
-    batch_size, channels, height, width = input_shape
-    out_height = (height - kernel_size + 2 * padding) // stride + 1
-    out_width = (width - kernel_size + 2 * padding) // stride + 1
-    return (batch_size, channels, out_height, out_width)
-
-
-def get_conv2d_output_shape(
-    input_shape, output_channels, kernel_size, stride, padding="same"
-):
-    # Calculate the output shape of the convolutional layer
-    batch_size, channels, height, width = input_shape
-
-    kernel_height, kernel_width, stride_height, stride_width, padding, _ = (
-        unpack_params(kernel_size, stride, padding)
-    )
-
-    # Transform padding from string to int
-    if isinstance(padding, str):
-        if padding == "valid":
-            padding = 0
-        elif padding == "same":
-            padding = (kernel_size - 1) // 2
-        else:
-            raise ValueError(f"Invalid padding type: {padding}")
-    elif isinstance(padding, int):
-        padding = padding
-    else:
-        raise ValueError(f"Invalid padding type: {padding}")
-
-    # Calculate the output height and width
-    out_height = (height - kernel_height + 2 * padding) // stride_height + 1
-    out_width = (width - kernel_width + 2 * padding) // stride_width + 1
-
-    return (batch_size, output_channels, out_height, out_width)
-
-
-def get_convtranspose2d_output_shape(
-    input_shape, output_channels, kernel_size, stride, padding="same"
-):
-    """
-    Compute the output shape of a ConvTranspose2d (a.k.a. deconvolution or upconvolution).
-
-    Parameters:
-        input_shape (tuple): (batch_size, in_channels, height, width)
-        output_channels (int): number of output channels
-        kernel_size (int or tuple): kernel size
-        stride (int or tuple): stride
-        padding (int or 'same' or 'valid'): padding
-        output_padding (int or tuple): additional size added to one side of the output
-
-    Returns:
-        tuple: (batch_size, output_channels, out_height, out_width)
-    """
-    batch_size, in_channels, height, width = input_shape
-
-    kernel_height, kernel_width, stride_height, stride_width, padding, _ = (
-        unpack_params(kernel_size, stride, padding)
-    )
-
-    # Handle padding
-    if isinstance(padding, str):
-        if padding == "valid":
-            padding = 0
-        elif padding == "same":
-            padding = (kernel_height - 1) // 2
-        else:
-            raise ValueError(f"Invalid padding type: {padding}")
-    elif isinstance(padding, int):
-        padding = padding
-    else:
-        raise ValueError(f"Invalid padding value: {padding}")
-
-    out_height = (height - 1) * stride_height - 2 * padding + kernel_height
-    out_width = (width - 1) * stride_width - 2 * padding + kernel_width
-
-    return (batch_size, output_channels, out_height, out_width)
+import torch
 
 
 def get_network_output_shape(input_shape, layers, print_all=False):
@@ -138,33 +13,13 @@ def get_network_output_shape(input_shape, layers, print_all=False):
         int: The output shape of the network
     """
     output_shape = input_shape
-    for layer in layers:
-        if hasattr(layer, "get_output_shape") and callable(
-            getattr(layer, "get_output_shape")
-        ):
-            output_shape = layer.get_output_shape(output_shape)
-        elif isinstance(layer, nn.Conv2d):
-            output_shape = get_conv2d_output_shape(
-                output_shape,
-                layer.out_channels,
-                layer.kernel_size,
-                layer.stride,
-                layer.padding,
-            )
-        elif isinstance(layer, nn.MaxPool2d):
-            output_shape = get_maxpool2d_output_shape(
-                output_shape, layer.kernel_size, layer.stride, layer.padding
-            )
-        elif isinstance(layer, nn.ConvTranspose2d):
-            output_shape = get_convtranspose2d_output_shape(
-                output_shape,
-                layer.out_channels,
-                layer.kernel_size,
-                layer.stride,
-                layer.padding,
-            )
 
-        if print_all:
-            print(f"{layer.__class__.__name__} output shape: {output_shape}")
+    with torch.no_grad():
+        x = torch.randn(*input_shape)
+        for layer in layers:
+            x = layer(x)
+            output_shape = x.shape
+            if print_all:
+                print(f"{layer.__class__.__name__} output shape: {output_shape}")
 
     return output_shape