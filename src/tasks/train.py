Fixed major bug in loss computation: wrong use of logits + missing attribute weights from tasks.

src/tasks/train.py
@@ -67,7 +67,11 @@ def run_training(config, model_path=None, cpu_override=False):
     if multi_gpu:
         model = nn.DataParallel(model)
 
-    loss_criterion = get_loss(trainer_config["loss"], class_weights)
+    loss_criterion = get_loss(
+        config=trainer_config["loss"],
+        class_weights=weights["class_weights"],
+        attribute_weights=weights["attribute_weights"],
+    )
 
     trainer = get_trainer(
         config,

src/utils/losses/__init__.py
@@ -5,7 +5,7 @@ from .losses import *
 from .losses_ae import *
 
 
-def get_loss(config, class_weights):
+def get_loss(config, class_weights=None, attribute_weights=None):
     loss_classes = get_classes_from_module(
         module_startswith="utils.losses", parent_class=nn.Module
     )
@@ -15,4 +15,6 @@ def get_loss(config, class_weights):
     loss_class = loss_classes.get(name)
     if loss_class is None:
         raise ValueError(f"Unknown loss: {name}")
-    return loss_class(class_weights=class_weights, **kwargs)
+    return loss_class(
+        class_weights=class_weights, attribute_weights=attribute_weights, **kwargs
+    )

src/utils/losses/losses.py
@@ -95,13 +95,18 @@ class AttributeLoss(nn.Module):
     def __init__(
         self,
         loss_lambda=1.0,
-        loss_criterion=F.binary_cross_entropy,
+        loss_criterion=F.binary_cross_entropy_with_logits,
         attribute_weights=None,
     ):
         super(AttributeLoss, self).__init__()
         self.loss_lambda = loss_lambda
         self.loss_criterion = loss_criterion
-        self.attribute_weights = attribute_weights
+        self.attribute_weights = None
+
+        if attribute_weights is not None:
+            self.attribute_weights = torch.tensor(
+                attribute_weights, dtype=torch.float32
+            )
 
     def forward(self, attribute_scores, attribute_targets):
         """
@@ -116,20 +121,12 @@ class AttributeLoss(nn.Module):
         """
         device = attribute_scores.device
         attribute_targets = attribute_targets.to(device)
-        attribute_scores = attribute_scores.clamp(
-            0, 1
-        )  # TODO: remove this, I don't like it
-        if self.attribute_weights is not None:
-            weights = torch.tensor(
-                self.attribute_weights, dtype=torch.float32, device=device
-            )
-            bce = F.binary_cross_entropy(
-                attribute_scores, attribute_targets, reduction="none"
-            )
-            weighted_bce = bce * weights
-            loss = weighted_bce.mean()
-        else:
-            loss = self.loss_criterion(attribute_scores, attribute_targets)
+
+        loss = self.loss_criterion(
+            attribute_scores,
+            attribute_targets,
+            pos_weight=self.attribute_weights,
+        )
 
         return self.loss_lambda * loss
 